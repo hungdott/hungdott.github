071fd53b7de389ee266f4cb54a2248459bc72ce4
hungdott <34940551+hungdott@users.noreply.github.com>
2018-06-26 23:23:06 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,14 +3,39 @@ const uid = require('uid')
 const $=require('jquery')
 const openStream=require('./openCamera')
 const playVideo=require('./playVideo')
-const config={host:'streambase.herokuapp.com', port: 443, secure: true, key: 'peerjs' }
+// Node Get ICE STUN and TURN list
+var options = {
+    host: "global.xirsys.net",
+    path: "/_turn/hungdo99ttbn",
+    method: "PUT",
+    headers: {
+        "Authorization": "Basic " + new Buffer("hungdo:06081f0c-7558-11e8-a620-264837236acc").toString("base64")
+    }
+};
+var httpreq = https.request(options, function(httpres) {
+    var str = "";
+    httpres.on("data", function(data){ str += data; });
+    httpres.on("error", function(e){ console.log("error: ",e); });
+    httpres.on("end", function(){ 
+        console.log("ICE List: ", str);
+    });
+});
+httpreq.end();
+
+const connection={
+    host:'streambase.herokuapp.com', 
+    port: 443, 
+    secure: true, 
+    key: 'peerjs',
+    config: options
+}
 
 function getPeer(){
     const id=uid(10);
     $('#peer1-id').append(id);
      return id;
  }
- const peer = Peer(getPeer(),config)
+ const peer = Peer(getPeer(),connection)
  console.log(peer);
 
 $('#btnCall').click(()=>{
